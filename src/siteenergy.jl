096863e5f231c2c8c42c0b49c5fbd0af3b8b211f
Christoph Ortner <c.ortner@warwick.ac.uk>
2021-05-29 22:08:05 -0700

    multi species site potential

--- src/siteenergy.jl
+++ src/siteenergy.jl
@@ -4,6 +4,8 @@ import JuLIP.Potentials: SitePotential
 
 # TODO: nasty hack - must fix this!!!
 
+cutoff(model::ACE.LinearACEModel) = cutoff(model.basis)
+
 cutoff(basis::ACE.SymmetricBasis) = cutoff(basis.pibasis)
 
 cutoff(basis::ACE.PIBasis) = cutoff(basis.basis1p)
@@ -20,27 +22,39 @@ cutoff(Rn::ACE.Rn1pBasis) = cutoff(Rn.R)
 `struct ACESitePotential` : wraps an ACE model into a JuLIP calculator
 """
 struct ACESitePotential{ENV, TM} <: SitePotential
-   model::TM
+   models::Dict{AtomicNumber, TM}
 end
 
 struct ACESitePotentialBasis{ENV, TM} <: JuLIP.MLIPs.IPBasis
-   model::TM  # model = basis
+   models::Dict{AtomicNumber, TM}  # model = basis
+   inds::Dict{AtomicNumber, UnitRange{Int}}
 end
 
 ACESiteCalc{ENV} = Union{ACESitePotential{ENV}, ACESitePotentialBasis{ENV}}
 
+Base.length(ipbasis::ACESitePotentialBasis) = sum(length, ipbasis.models)
 
-Base.length(ipbasis::ACESitePotentialBasis) = length(ipbasis.model)
+cutoff(V::ACESiteCalc) =  maximum(cutoff, values(V.models))
 
-cutoff(V::ACESitePotential) =  cutoff(V.model.basis)
-cutoff(V::ACESitePotentialBasis) =  cutoff(V.model)
+function ACESitePotential(models::Dict{Symbol, TM}, 
+                          ENV = AtomicEnvironment) where {TM} 
+   mods = Dict([ AtomicNumber(sym) => mo for (sym, mo) in models]...)
+   return ACESitePotential(mods, ENV)
+end
 
+ACESitePotential(models::Dict{AtomicNumber, TM}, ENV = AtomicEnvironment) where {TM} = 
+     ACESitePotential{ENV, TM}(models)
 
-ACESitePotential(model, ENV = AtomicEnvironment) = 
-     ACESitePotential{ENV, typeof(model)}(model)
-
-basis(V::ACESitePotential, ENV = AtomicEnvironment) = 
-      ACESitePotentialBasis{ENV, typeof(V.model.basis)}(V.model.basis)
+function basis(V::ACESitePotential{ENV}) where ENV 
+   models = Dict( [sym => model.basis for (sym, model) in V.models]... )
+   inds = Dict{AtomicNumber, UnitRange{Int}}()
+   i0 = 0
+   for (z, mo) in models
+      inds[z] = (i0+1):(i0+length(mo))
+      i0 += length(mo)
+   end
+   return ACESitePotentialBasis{ENV, valtype(models)}(models, inds)
+end
 
 function environment(V::ACESiteCalc{<: AtomicEnvironment}, 
                      at::Atoms, nlist, i::Integer) where {TM}
@@ -56,30 +70,36 @@ environment(V::ACESiteCalc{<: AtomicEnvironment},
 
 JuLIP.alloc_temp(V::ACESiteCalc, N::Integer) = 
       ( JuLIP.Potentials.alloc_temp_site(N)..., 
-        tmpmodel = alloc_temp(V.model)
+        tmpmodel = Dict([z => alloc_temp(mo) for (z, mo) in V.models]...)
       )
 
 evaluate!(tmp, V::ACESitePotential, Rs, Zs, z0) = 
-      evaluate!(tmp.tmpmodel, V.model, environment(V, Rs, Zs, z0)).val
+      evaluate!(tmp.tmpmodel[z0], V.models[z0], environment(V, Rs, Zs, z0)).val
 
-evaluate!(B, tmp, V::ACESitePotentialBasis, Rs, Zs, z0) = 
-      evaluate!(B, tmp.tmpmodel, V.model, environment(V, Rs, Zs, z0))
-      
+function evaluate!(B, tmp, V::ACESitePotentialBasis, Rs, Zs, z0) 
+   fill!(B, 0)
+   Bview = (@view B[V.inds[z0]])
+   evaluate!(Bview, tmp.tmpmodel[z0], V.models[z0], environment(V, Rs, Zs, z0))
+   return B 
+end
+
 
 alloc_temp_d(V::ACESiteCalc, N::Integer) = 
       ( JuLIP.Potentials.alloc_temp_site(N)...,
         dV = zeros(JVec{Float64}, N), 
-        tmpdmodel = alloc_temp_d(V.model, N)
+        tmpdmodel = Dict([ z => alloc_temp_d(mo, N) for (z, mo) in V.models ]...)
       )
 
 evaluate_d!(dV, tmpd, V::ACESitePotential, Rs, Zs, z0) = 
-      ACE.grad_config!(dV, tmpd.tmpdmodel, V.model, environment(V, Rs, Zs, z0))
+      ACE.grad_config!(dV, tmpd.tmpdmodel[z0], V.models[z0], environment(V, Rs, Zs, z0))
 
-evaluate_d!(dB, tmpd, V::ACESitePotentialBasis, Rs, Zs, z0) = 
-      evaluate_d!(dB, tmpd.tmpdmodel, V.model, environment(V, Rs, Zs, z0))
+function evaluate_d!(dB, tmpd, V::ACESitePotentialBasis, Rs, Zs, z0)
+   fill!(dB, zero(eltype(dB)))
+   dBview = (@view dB[V.inds[z0], :])
+   evaluate_d!(dBview, tmpd.tmpdmodel[z0], V.models[z0], environment(V, Rs, Zs, z0))
+   return dB
+end
                 
                 
-
-# ----------------- similar interface for the basis 
 
 
--- test/test_siteenergy.jl
+++ test/test_siteenergy.jl
@@ -23,11 +23,13 @@ ACE.init1pspec!(B1p)
 φ = ACE.Invariant()
 pibasis = PIBasis(B1p, ord, maxdeg; property = φ)
 basis = SymmetricBasis(pibasis, φ)
-c = randn(length(basis))
-model = ACE.LinearACEModel(basis, c; evaluator = :standard)
+cTi = randn(length(basis))
+cAl = randn(length(basis))
+models = Dict(:Ti => ACE.LinearACEModel(basis, cTi; evaluator = :standard), 
+              :Al => ACE.LinearACEModel(basis, cAl; evaluator = :standard) )
 
 @info("Convert LinearACEModel into an ACESitePotential")
-V = ACEatoms.ACESitePotential(model)
+V = ACEatoms.ACESitePotential(models)
 
 ##
 
@@ -45,9 +47,10 @@ nlist = neighbourlist(at, cutoff(V))
 env, _ = ACEatoms.environment(V, at, nlist, 2)
 Js, Rs, Zs = JuLIP.Potentials.neigsz(nlist, at, 2)
 z0 = at.Z[2]
+sym = chemical_symbol(z0)
 env1 = ACEatoms.environment(V, Rs, Zs, z0)
 ACEbase._allfieldsequal(env1, env)
-v1 = evaluate(model, env).val
+v1 = evaluate(models[sym], env).val
 v2 = evaluate(V, Rs, Zs, z0)
 println(@test v1 ≈ v2)
 energy(V, at)
@@ -55,7 +58,7 @@ energy(V, at)
 ##
 @info("Test that grad_config(model) ≈ evaluate_d(site-potential)")
 dEs = zeros(JVecF, length(env))
-dv1 = ACE.grad_config!(dEs, ACE.alloc_temp_d(V.model, env), V.model, env)
+dv1 = ACE.grad_config!(dEs, ACE.alloc_temp_d(V.models[z0], env), V.models[z0], env)
 dv2 = JuLIP.evaluate_d!(dEs, ACE.alloc_temp_d(V, length(env)), V, Rs, Zs, z0)
 dv3 = evaluate_d(V, Rs, Zs, z0)
 
@@ -73,6 +76,8 @@ println(@test JuLIP.Testing.fdtest(V, at))
 @info("Check also the ACESitePotentialBasis interface")
 
 ipbasis = ACEatoms.basis(V)
+
+energy(ipbasis, at)
 
 @info("  ... energy")
 val1 = energy(V, at)
