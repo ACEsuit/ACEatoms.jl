6551e9d2d60930c45a39c4f877b12a43df30ee5a
Christoph Ortner <c.ortner@warwick.ac.uk>
2021-10-13 13:35:54 -0700

    draft of getbondenv

--- src/bonds/calculators.jl
+++ src/bonds/calculators.jl
@@ -1,9 +1,14 @@
 
-
+"""
+Given a bond (i, j) it has an environment {k} which could be 
+cylindrical, ellipsoidal, ...  
+The ball B(ri, rcut_env) must be a superset of this environment.
+"""
+function cutoff_env end 
 
 function prepare_bondcalc(model, at)
    rcut_b = cutoff(model)
-   rcut_e = cutoff_env(model)
+   rcut_e = cutoff_env(model)    #  CAREFUL HOW WE DEFINE THIS!!!!!
    nlist_b = neighbourlist(at, rcut_b)
    nlist_e = neighbourlist(at, rcut_e)
    # consider filtering the nlist_b so that each bond 
@@ -11,27 +16,29 @@ function prepare_bondcalc(model, at)
    return nlist_b, nlist_e
 end
 
-function get_bond_env(i, rri, j, rrj, nlist_e, at)
-   Jsi, Rsi = neigs(nlist_e, i)
-   Jsj, Rsj = neigs(nlist_e, j)
+function get_bond_env(i0, rr0, i1, rr1, nlist_e, bondmodel)
+   Js0, Rs0 = neigs(nlist_e, i0)  # superset of actual environment
    Js = Int[] 
    Xs = JVecF[] 
    # go through all, fill them into a NearestNeighbour datastructure 
-   rrm = 0.5 * (rri + rrj)
-   for (Js0, Rs0, rr0) in ( (Jsi, Rsi, rri), (Jsj, Rsj, rrj) )
-      for (j, rr) in zip(Js0, Rs0)
-         # recenter the rr vector relative to bond mid-point
-         rr = rr + rr0 - rrm
-         # todo: if already in list skip 
-         # todo: add it to the list 
-         # convert it to an ACEState! add other stuff, such as species, etc. ???
-         X = State(rr = rr, be = :e)
-         if filter(bondmodel, X
-            push!(Js, j)
-            push!(Xs, X)
-         end 
+   # FIX THIS : 0.5 -> lambda in bondmodel
+   rrm = 0.5 * (rr0 + rr1)
+   for (j, rr_ij) in zip(Js0, Rs0)
+      # recenter the rr vector relative to bond mid-point
+      rr_j = rr0 + rr_ij   # absolute position of that env atom 
+      rr_mj = rr_j - rrm   # position relative  to bond center point 
+      # convert it to an ACEState add other stuff, such as species, etc. ???
+      X = State(rr = rr_mj, be = :e, rr0 = rr1 - rr0)
+      if filter(bondmodel, X)
+         push!(Js, j)
+         push!(Xs, X)
       end 
    end
+
+   # add the center-bond to the config 
+   push!(Js, 0)
+   push!(cfg.Xs, State(rr = rr1 - rr0, be = :b, rr0 = rr1 - rr0))
+
    return Js, ACEConfig(Xs)
 end
 
@@ -41,18 +48,16 @@ function energy(model::ACEBondPotential, at)
    nlist_b, nlist_e = prepare_bondcalc(model, at)
 
    for i = 1:length(at) 
-      Js, Rs = neigs(nlist, i)
-      for (j, rrij) in Rs 
+      Js, Rs = neigs(nlist_b, i)
+      for (j, rr_ij) in Rs 
          # todo: we want something like this but it is not obvious 
-         # how to implement it 
-         # if j > i; continue; end 
+         # how to implement it : if j > i; continue; end 
 
          # figure out what rri, rrj means in this context!?!?! not obvious 
          # again!!! 
-         Js_env, cfg_env = get_bond_env(i, rri, j, rrj, nlist_e, at)
-
-         # add the center-bond to the config 
-         push!(cfg.Xs, State(rr = rrij, be = b))
+         rr_i = at.X[i] 
+         rr_j = rr_i + rr_ij  # relative position; j could be outside the cell
+         Js_env, cfg_env = get_bond_env(i, rr_i, j, rr_j, nlist_e, bondmodel)
          
          # evaluate the ace bond model and add to total energy 
          E += evaluate(model, cfg) 
