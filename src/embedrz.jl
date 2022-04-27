24cc567d6225bd976af5439a5173a3b93996a820
Christoph Ortner <christophortner0@gmail.com>
2022-04-27 13:14:18 -0700

    gradients for EMbedRZ

--- src/embedrz.jl
+++ src/embedrz.jl
@@ -2,31 +2,53 @@
 
 import ACE, ACEbase
 
-import ACE: SList, i2val, val2i, get_spec, symbols, indexrange, get_index, degree 
+import ACE: SList, i2val, val2i, get_spec, symbols, indexrange, get_index, degree, 
+            evaluate, evaluate_d, evaluate_ed 
 
 using ACE: @λ, chain, GetNorm, B1pComponent
 
-# -------------------------
+# ---------------------------------------------------------------------------
+# this is a bit of a nasty hack, still need to figure out how to do this 
+# in a more systematic way
+
+import ACE.Transforms: StaticGet, get_symbols, getval, getval_d, grad_type_dP
+import ACE: rrule_evaluate, DState
+using LinearAlgebra: norm 
+
+struct GetNormMu{RSYM, SSYM} <: StaticGet end 
+
+getval(X, ::GetNormMu{RSYM, SSYM}) where {RSYM, SSYM} = 
+      (norm(getproperty(X, RSYM)), getproperty(X, SSYM))
+
+function getval_d(X, ::GetNormMu{RSYM}) where {RSYM}
+   x = getproperty(X, RSYM)
+   return DState( NamedTuple{(RSYM,)}( (x/norm(x),) ) )
+end 
+
+get_symbols(::GetNormMu{RSYM, SSYM}) where {RSYM, SSYM} = (RSYM, SSYM)
+
+function rrule_evaluate(dP, _val::GetNormMu{RSYM}, X) where {RSYM}
+   rr = getproperty(X, RSYM)
+   drr = rr/norm(rr)
+   TDX = ACE.dstate_type(X)
+   tdx = g -> TDX(DState( NamedTuple{(RSYM,)}( (g,) ) ))
+   return [ tdx(drr * dP[n]) for n = 1:length(dP) ]
+end
 
 
-struct EmbedRZ{RSYM, SSYM, TPR, T}
+# ---------------------------------------------------------------------------
+
+
+struct EmbedRZ{TPR, T}
    Pr::TPR
    embeddings::Dict{Any, Matrix{T}}
 end
 
-function EmbedRZ(rsym::Symbol, ssym::Symbol, Pr::TPR, embeddings::Dict
-                )  where {TPR}
+function EmbedRZ(Pr::TPR, embeddings::Dict)  where {TPR}
    T = eltype(first(embeddings)[2])
    embeddings = Dict{Any, Matrix{T}}(embeddings)
-   return EmbedRZ{rsym, ssym, TPR, T}(Pr, embeddings)
+   return EmbedRZ(Pr, embeddings)
 end
-
-
-_rsym(basis::EmbedRZ{RSYM, SSYM}) where {RSYM, SSYM} = RSYM
-_ssym(basis::EmbedRZ{RSYM, SSYM}) where {RSYM, SSYM} = SSYM
-
-_rr(basis::EmbedRZ, X) = getproperty(X, _rsym(basis))
-_μ(basis::EmbedRZ, X) = getproperty(X, _ssym(basis))
 
 
 function embed_nl_spec1(maxdeg::Integer, wl = 1.5)
@@ -46,18 +68,29 @@ function embedrz(Pr,
                  spec::AbstractVector{<: NamedTuple}, 
                  embeddings::Dict; 
                  rrsym = :rr, ssym = :mu, label = "Rnl")
-   getval = GetNorm{rrsym}() 
+   getval = GetNormMu{rrsym, ssym}() 
    degrees = zeros(Int, length(spec))
-   Rnl = EmbedRZ(rrsym, ssym, Pr, embeddings)
+   Rnl = EmbedRZ(Pr, embeddings)
    return B1pComponent(Rnl, getval, spec, degrees, label)
 end
 
 
-function evaluate(basis::EmbedRZ, X)
-   P = evaluate(basis.Pr, _rr(basis, X))
-   uP = basis.embeddings[_μ(basis, X)] * P 
+function evaluate(basis::EmbedRZ, r_μ::Tuple{<: AbstractFloat, Symbol})
+   r, μ = r_μ
+   P = evaluate(basis.Pr, r)
+   uP = basis.embeddings[μ] * P 
    ACE.release!(P)
    return uP 
 end
 
+
+function evaluate_ed(basis::EmbedRZ, r_μ::Tuple{<: AbstractFloat, Symbol})
+   r, μ = r_μ
+   P, dP = evaluate_ed(basis.Pr, r)
+   uP = basis.embeddings[μ] * P 
+   udP = basis.embeddings[μ] * dP 
+   ACE.release!(P)
+   ACE.release!(dP)
+   return uP, udP 
+end
 
--- test/test_embedrz.jl
+++ test/test_embedrz.jl
@@ -3,8 +3,8 @@ using ACE, ACEatoms, JuLIP, LinearAlgebra, ACEbase, Test
 using ACEatoms: AtomState
 using ACEbase.Testing
 using ACE: evaluate, evaluate_d, State
+using ACE: @λ
 using StaticArrays
-
 using ACEatoms: embed_nl_spec1, embedrz
 
 ##
@@ -13,10 +13,10 @@ using ACEatoms: embed_nl_spec1, embedrz
 maxdeg = 10
 r0 = 1.0
 rcut = 3.0
-trans = PolyTransform(1, r0)
+trans = @λ r -> 2/(1+r)
 Qn = ACE.transformed_jacobi(maxdeg, trans, rcut)
 # this is a bad temporary hack, I need to figure out a way to get this 
-# more easily ...
+# more easily ... it's all happening, just needs a bit of time.
 len_Qn = length(Qn.F[end])
 
 # now embed it 
@@ -36,3 +36,27 @@ rand_X() = State( rr = ACE.rand_sphere() * (r0 + rand() * (rcut-r0)),
 
 X = rand_X()
 evaluate(Rnl, X)
+evaluate_d(Rnl, X)
+
+## basic tests 
+
+@info("check correct application of the embedding, correctness of `evaluate`")
+for ntest = 1:30 
+   X = rand_X()
+   print_tf(@test(embeddings[X.mu] * evaluate(Qn, norm(X.rr)) ≈ evaluate(Rnl, X)))
+end
+
+##
+
+using ACEbase.Testing: fdtest 
+@info("Finite difference test")
+for ntest = 1:30 
+   u = randn(length(Rnl))
+   dX = ACE.DState( rr = ACE.rand_sphere() )
+   F = t -> dot(u, evaluate(Rnl, X + t * dX))
+   dF = t -> (g = evaluate_d(Rnl, X + t * dX);  dot(dX.rr, sum(u[n] * g[n].rr for n = 1:length(g)))) 
+   print_tf(@test( fdtest(F, dF, 0.0; verbose=false) ))
+end
+
+##
+
